f50a369d909ebdcf885a217ebd33d181b1827d0e
Hannah Lee <013hannah.a.lee@gmail.com>
2021-04-19 16:21:29 -0500

    updated folder routes, worked on new routes in server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
-const viewRoutes = require('./routes/view');
-const apiRoutes = require('./routes/api');
+const notesData = require('./db/db.json');
+const fs = require('fs');
+// const viewRoutes = require('./routes/view');
+// const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = 4600;
@@ -10,8 +12,36 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use('/', viewRoutes);
+// app.use('/', viewRoutes);
+// app.use('/api', apiRoutes);
 
-app.use('/api', apiRoutes);
+//API Routes
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+       let notes = JSON.parse(data);
+       notes.push(req.body);
+
+       fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+        console.log(err);
+        // res.json(notes)
+        })
+    });
+});
+
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// If no matching route is found default to home
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
